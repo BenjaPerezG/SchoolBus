d998bf4580690cbc29a0b28f7b1c571dc87966cc
Rabinovich1825 <baperez@miuandes.cl>
2018-05-30 18:09:42 -0400

    DAO updated

--- app/src/main/java/com/example/rabinovich/schoolbus/Database/AppDao.java
+++ app/src/main/java/com/example/rabinovich/schoolbus/Database/AppDao.java
@@ -1,7 +1,10 @@
 package com.example.rabinovich.schoolbus.Database;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Query;
+
+import java.util.List;
 
 /**
  * Created by Rabinovich on 5/23/2018.
@@ -9,20 +12,39 @@ import android.arch.persistence.room.Query;
 
 @Dao
 public interface AppDao {
-    //User queries
+//////User queries//////////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM user WHERE user.id = :user_id")
+    User getUserById(int user_id);
 
-    //Guardian queries
+//////Guardian queries//////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM guardian WHERE guardian.id = :guardian_id")
+    Guardian getGuardianById(int guardian_id);
 
-    //Driver queries
+//////Driver queries////////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM driver WHERE driver.id = :driver_id")
+    Driver getDriverById(int driver_id);
 
-    //Bus queries
+//////Bus queries///////////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM bus WHERE bur.id = :bus_id")
+    Bus getBusById(int bus_id);
 
-    //Trip queries
+//////Trip queries//////////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM trip")
+    List<Trip> getAllTrips();
 
-    //Student queries
+    @Query("SELECT * FROM trip WHERE trip.id = :trip_id")
+    Trip getTripById(int trip_id);
 
-    //Stop queries
+//////Student queries///////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM student WHERE student.id = :student_id")
+    Student getStudentById(int student_id);
 
-    //Mixed queries
-    @Query("SELECT * FROM s student, t trip, tr tripstudent WHERE s.id = tr.student_id and tr.trip_id = t.id)
+//////Stop queries//////////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT * FROM stop WHERE stop.id = :stop_id")
+    Stop getStopById(int stop_id);
+
+//////Mixed queries/////////////////////////////////////////////////////////////////////////////////
+    @Query("SELECT s FROM s student, t trip, tr tripstudent " +
+            "WHERE s.id = tr.student_id and tr.trip_id = :trip_id")
+    LiveData<List<Student>> getStudentsByTrip(int trip_id);
 }
