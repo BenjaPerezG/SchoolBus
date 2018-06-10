cd02632c9db50fbfb8263772f997abdbd17053c8
Sebastian Baixas <sbaixas@gmail.com>
2018-06-10 02:40:44 -0400

    User pushed and pulled from the database successfully

--- app/src/main/java/com/example/rabinovich/schoolbus/Fragments/AdminRegistrationFragment.java
+++ app/src/main/java/com/example/rabinovich/schoolbus/Fragments/AdminRegistrationFragment.java
@@ -2,17 +2,21 @@ package com.example.rabinovich.schoolbus.Fragments;
 
 
 import android.annotation.SuppressLint;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rabinovich.schoolbus.Database.User;
 import com.example.rabinovich.schoolbus.Database.UserViewModel;
@@ -67,11 +71,21 @@ public class AdminRegistrationFragment extends Fragment {
     }
 
     private void Register(){
+
         User user = new User();
         user.setFirst_name(firstNameEditText.getText().toString());
         user.setLast_name(lastNameEditText.getText().toString());
         user.setEmail(emailEditText.getText().toString());
         user.setPassword(passwordEditText.getText().toString());
+        user.setIsAdmin(true);
         userViewModel.insert(user);
+        final User user1 = new User();
+        userViewModel.getUserByCredentials(emailEditText.getText().toString(),passwordEditText.getText().toString()).observe(getActivity(), new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                user1.setFirst_name(user.getFirst_name());
+                Log.d("usertry", user1.getFirst_name());
+            }
+        });
     }
 }
