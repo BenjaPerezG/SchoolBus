81661d71f045ccc992fa36bdc1288e9832cf0ba0
Rabinovich1825 <baperez@miuandes.cl>
2018-06-10 00:13:37 -0400

    Trip view model ready

--- app/src/main/java/com/example/rabinovich/schoolbus/Database/TripViewModel.java
+++ app/src/main/java/com/example/rabinovich/schoolbus/Database/TripViewModel.java
@@ -2,8 +2,10 @@ package com.example.rabinovich.schoolbus.Database;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 
 import java.security.PublicKey;
+import java.util.List;
 
 //How to use: in UserViewModel.java
 public class TripViewModel extends AndroidViewModel {
@@ -14,4 +16,12 @@ public class TripViewModel extends AndroidViewModel {
         super(application);
         mRepository = new TripRepository(application);
     }
+
+    LiveData<Trip> getTripById(int id){ return mRepository.getTripById(id); }
+    LiveData<List<Trip>> getAllTrips(){ return mRepository.getAllTrips(); }
+    LiveData<List<Trip>> getTripsByDriverId(int driver_id){ return mRepository.getTripsByDriverId(driver_id); }
+    LiveData<List<Trip>> getTripsByBusId(int bus_id){ return  mRepository.getTripsByBusId(bus_id); }
+    LiveData<List<Trip>> getmTripsByDate(String date){ return mRepository.getTripsByDate(date); }
+
+    public void insert(Trip trip){ mRepository.insert(trip); }
 }
