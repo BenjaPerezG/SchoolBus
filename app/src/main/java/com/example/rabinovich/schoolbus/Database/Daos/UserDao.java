e09599867387e2d1ec7c92218bd167b2b0663f93
Rabinovich1825 <baperez@miuandes.cl>
2018-06-09 20:41:53 -0400

    @dao deleted (there where two)

--- app/src/main/java/com/example/rabinovich/schoolbus/Database/Daos/UserDao.java
+++ app/src/main/java/com/example/rabinovich/schoolbus/Database/Daos/UserDao.java
@@ -7,7 +7,7 @@ import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
 import com.example.rabinovich.schoolbus.Database.User;
-@Dao
+
 @Dao
 public interface UserDao {
     @Insert
