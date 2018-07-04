cdfa42bedb496c07f60aa8102f122cd62dc68702
Sebastian Baixas <sbaixas@gmail.com>
2018-07-03 23:01:32 -0400

    Guardians can call the drivers of their students if they have a trip that day

--- app/src/main/java/com/example/rabinovich/schoolbus/Fragments/GuardianStudentFragment.java
+++ app/src/main/java/com/example/rabinovich/schoolbus/Fragments/GuardianStudentFragment.java
@@ -15,17 +15,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.rabinovich.schoolbus.Adapters.StudentAdapter;
+import com.example.rabinovich.schoolbus.Database.Bus;
 import com.example.rabinovich.schoolbus.Database.StopViewModel;
 import com.example.rabinovich.schoolbus.Database.Student;
 import com.example.rabinovich.schoolbus.Database.StudentViewModel;
+import com.example.rabinovich.schoolbus.Database.Trip;
+import com.example.rabinovich.schoolbus.Database.TripStudent;
+import com.example.rabinovich.schoolbus.Database.TripStudentViewModel;
+import com.example.rabinovich.schoolbus.Database.TripViewModel;
+import com.example.rabinovich.schoolbus.Database.User;
 import com.example.rabinovich.schoolbus.Database.UserViewModel;
 import com.example.rabinovich.schoolbus.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,18 +45,25 @@ public class GuardianStudentFragment extends Fragment {
     StudentViewModel studentViewModel;
     UserViewModel userViewModel;
     StopViewModel stopViewModel;
+    TripViewModel tripViewModel;
+    TripStudentViewModel tripStudentViewModel;
     ListView listView;
+    List<Trip> trips;
+    List<TripStudent> tripStudents;
+    List<User> drivers;
 
     int id;
 
 
 
     @SuppressLint("ValidFragment")
-    public GuardianStudentFragment(StudentViewModel studentViewModel, UserViewModel userViewModel, StopViewModel stopViewModel) {
+    public GuardianStudentFragment(StudentViewModel studentViewModel, UserViewModel userViewModel, StopViewModel stopViewModel, TripViewModel tripViewModel, TripStudentViewModel tripStudentViewModel) {
         // Required empty public constructor
         this.studentViewModel = studentViewModel;
         this.userViewModel = userViewModel;
         this.stopViewModel = stopViewModel;
+        this.tripViewModel = tripViewModel;
+        this.tripStudentViewModel = tripStudentViewModel;
     }
 
 
@@ -62,6 +77,29 @@ public class GuardianStudentFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        userViewModel.getUsersByUserType("driver").observe(getActivity(), new Observer<List<User>>() {
+            @Override
+            public void onChanged(@Nullable List<User> ousers) {
+                drivers = ousers;
+            }
+        });
+
+        tripViewModel.getAllTrips().observe(getActivity(), new Observer<List<Trip>>() {
+            @Override
+            public void onChanged(@Nullable List<Trip> oTrips) {
+                trips = oTrips;
+            }
+        });
+
+        tripStudentViewModel.getAllTripStudents().observe(getActivity(), new Observer<List<TripStudent>>() {
+            @Override
+            public void onChanged(@Nullable List<TripStudent> oTripStudents) {
+                tripStudents = oTripStudents;
+            }
+        });
+
 
         loginPreferences = this.getActivity().getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE);
         id = loginPreferences.getInt("userId",0);
@@ -85,7 +123,7 @@ public class GuardianStudentFragment extends Fragment {
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        GuardianShowStudentFragment guardianShowStudentFragment = new GuardianShowStudentFragment(studentViewModel, userViewModel,stopViewModel);
+                        GuardianShowStudentFragment guardianShowStudentFragment = new GuardianShowStudentFragment(studentViewModel, userViewModel,stopViewModel, drivers,trips, tripStudents);
                         Bundle arguments = new Bundle();
                         TextView id_student = (TextView) view.findViewById(R.id.student_id);
                         Integer current_id = Integer.parseInt(id_student.getText().toString());
@@ -97,8 +135,6 @@ public class GuardianStudentFragment extends Fragment {
                         transaction.addToBackStack(null);
 
                         transaction.commit();
-
-
                     }
                 });
                 StudentAdapter adapter = new StudentAdapter(students, getContext(), userViewModel);
